9f899862ce9ab28aa3b034cfb02df06902b01aa9
Jval <jval2@pdx.edu>
2022-10-18 17:11:13 -0700

    *

--- web_scraper_start.R
+++ web_scraper_start.R
@@ -1,16 +1,29 @@
-install.packages("rvest")
+#install.packages("rvest")
 library(rvest)
 library(tidyverse)
 
 cherry_data <- c()
 tbls_ls <- c()
-for (X in 1:417){
-  cherry_blossom <- read_html(paste("https://www.cballtimeresults.org/performances?division=Overall+Women&page=",X,"&section=10M&sex=W&utf8=%E2%9C%93&year=2022",sep=""))
-  tbls_ls <- as.data.frame(cherry_blossom %>%
-                 html_table(fill = TRUE))
-  cherry_data <- rbind(cherry_data, tbls_ls)
+for (X in 1:500){
+  for (Y in 1973:2019){
+      cherry_blossom <- read_html(paste("https://www.cballtimeresults.org/performances?division=Overall+Women&page=",X,"&section=10M&sex=&utf8=%E2%9C%93&year=",Y,sep=""))
+      tbls_ls <- as.data.frame(cherry_blossom %>%
+                                 html_table(fill = TRUE))
+      cherry_data <- rbind(cherry_data, tbls_ls)
+    }
 }
 
 head(cherry_data)
+
+library(readr)
+library(ggplot2)
+library(dplyr)
+
+mendata <- read_csv("C:/Users/justin/OneDrive/School/Stat 510 consulting/mendata.csv")
+
+mean_race_time = c()
+mean_time = c()
+
+
 
 
