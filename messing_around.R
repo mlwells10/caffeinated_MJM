57079fe1fadb5a0253891fde94ddbde8c4fd8c48
Jval <jval2@pdx.edu>
2022-10-18 22:53:11 -0700

    new_newdata which removed some NAs and did a plot of the mean race times

---
+++ messing_around.R
@@ -0,0 +1,55 @@
+
+library(tidyverse)
+library(readr)
+library(ggplot2)
+library(dplyr)
+
+
+
+
+Wdata <- read_csv("newdata.csv")
+View(newdata)
+
+
+Wdata$Age <- as.numeric(Wdata$Age )
+newdata <- Wdata %>% 
+  filter(!is.na(Wdata$Age))
+newdata$Time <- strptime(newdata$Time, "%H:%M:%S")
+newdata$Time <- hms::as_hms(newdata$Time)
+
+head(newdata)
+
+
+
+new_newdata <- newdata %>% 
+  filter(is.na(Time) == F)
+
+
+
+
+
+
+
+
+
+
+
+x <- new_newdata %>%
+  group_by(Race) %>% 
+  summarize(mean_run = mean(Time)) %>% 
+  print(n=Inf)
+
+
+ggplot(new_newdata, aes(x = x$mean_run, y=x$Race)) +
+  geom_point() +
+  scale_x_log10()
+
+df_x <- as.data.frame(x)
+
+ggplot(df_x, aes(x = Race, y=mean_run)) +
+  geom_point() 
+  
+
+new_newdata %>% 
+  filter(Race == 1978) %>% 
+  print(n=Inf)
