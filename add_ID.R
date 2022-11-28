099fb10a3e9bae01cb45803898d028b38b42c377
Michael Wells <mlwells@pdx.edu>
2022-11-27 17:51:38 -0800

    added ID column to data

---
+++ add_ID.R
@@ -0,0 +1,26 @@
+rm(list = ls())
+setwd('~/caffeinated_MJM/')
+
+df = read.csv('mendataclean.csv')
+dg = read.csv('newdata.csv')
+gender = rep('M',nrow(df))
+cbind(df,gender)
+gender = rep('F',nrow(dg))
+cbind(dg,gender)
+df = rbind(df,dg)
+df = df[df$Hometown!='NR',]
+name.hometown = unique(df[,c('Name','Hometown')])
+
+k=1
+ID = rep(0,nrow(df))
+for(k in 1:nrow(name.hometown))
+{
+  id = name.hometown[k,]
+  #df.id = df[df$Name%in%id[1]&df$Hometown%in%id[2],]
+  ID[df$Name%in%id[1]&df$Hometown%in%id[2]] = k
+  if(k%%1000==0)
+    print(k)
+}
+
+df = cbind(df,ID)
+write.csv(df,'data/combined_data_with_ID.csv',row.names=F)
